b5d95550b589954767c04ee3d72ae6fa4464761d
carney520 <carney520@hotmail.com>
2019-03-05 12:28:58 +0800

    fork convenional-changelog-angular 支持emoji

---
+++ parser-opts.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  headerPattern: /^(?:.+\s)?(\w*)(?:\((.*)\))?: (.*)$/,
+  headerCorrespondence: [
+    `type`,
+    `scope`,
+    `subject`
+  ],
+  noteKeywords: [`BREAKING CHANGE`],
+  revertPattern: /^(?:.+\s)?revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+  revertCorrespondence: [`header`, `hash`]
+}
